734ecbd6f42c2e4063299f7712ada2fdae9b1f47
kalchurrrrr <nikolaykalchu@gmail.com>
2023-11-27 21:00:52 +0300

    Выполнены пункты до 2.4

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +29,31 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            // Process your updates here
+            logger.info("Обработка обновления: {}", update);
+
+            if (update.message() != null && update.message().text() != null) {
+                if (update.message().text().equalsIgnoreCase("/start")) {
+                    sendMessage(update.message().chat().id(), "Бот успешно активирован!");
+                }
+                else {
+                    if (update.message().text().equalsIgnoreCase("/help")) {
+                        String helpText = "Тут выводится информация о том, как пользоваться ботом";
+                        sendMessage(update.message().chat().id(), helpText);
+                    }
+                }
+            }
         });
+
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+
+    private void sendMessage(Long chatId, String text) {
+        SendResponse sendResponse = telegramBot.execute(new SendMessage(chatId, text));
+
+        if (!sendResponse.isOk()) {
+            logger.error("Ошибка при отправке сообщения: {}", sendResponse);
+        }
     }
 
 }
